2f8748f8b37865cdbe67b5d5fc885064e3e2e2a8
root <jagadesh.bhimineni@unh.edu>
2025-04-29 23:52:30 -0500

    Initial commit

---
+++ src/main/java/com/example/Rest_Kafka_POC/RestKafkaPocApplication.java
@@ -0,0 +1,15 @@
+package com.example.Rest_Kafka_POC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestKafkaPocApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RestKafkaPocApplication.class, args);
+
+    }
+
+}
